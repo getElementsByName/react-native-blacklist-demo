08887ede0359f581aaf4ba9f19961b3d4554a9c6
getElementsByName <korywh@gmail.com>
2018-11-04 10:17:14 +0900

    add comment

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,8 +2,9 @@ const blacklist = require('metro-config/src/defaults/blacklist')
 
 module.exports = {
   resolver: {
-    // unstable - Each react-native versions has some difference APIs
-    // https://github.com/facebook/react-native/blob/7a69f1aa272a9b71755033a80f6f4aa5e9dcbaf6/local-cli/util/Config.js#L71
+    // [unstable] Each react-native versions has some difference APIs
+    // react-native/cli: https://github.com/facebook/react-native/blob/7a69f1aa272a9b71755033a80f6f4aa5e9dcbaf6/local-cli/util/Config.js#L71
+    // metro-config: https://github.com/facebook/metro/blob/master/packages/metro-config/src/defaults/index.js
     blacklistRE: blacklist([/packages\/.*\/node_modules\/react-native\/.*/])
   },
 }
